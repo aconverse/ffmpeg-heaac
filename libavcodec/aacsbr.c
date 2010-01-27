b573aaf25ccae3e04ead2a5d4c80f6c9501a607f
Alex Converse <alex.converse@gmail.com>
2010-01-26 19:58:26 -0500

    Replace t_HFAdj with ENVELOPE_ADJUSTMENT_OFFSET.

--- libavcodec/aacsbr.c
+++ libavcodec/aacsbr.c
@@ -1355,33 +1355,32 @@ static int sbr_hf_gen(AACContext *ac, SpectralBandReplication *sbr,
 static int sbr_x_gen(SpectralBandReplication *sbr,
                       float X[32][64][2], float X_low[32][40][2], float Y[2][64][40][2], int ch) {
     int k, l;
-    const int t_HFAdj = ENVELOPE_ADJUSTMENT_OFFSET;
     const int l_f = 32;
     const int l_Temp = FFMAX(2*sbr->data[ch].t_env_num_env_old - l_f, 0); //FIXME hack to make l_Temp initialize to zero
     memset(X, 0, 32*sizeof(*X));
     for (k = 0; k < sbr->k[4]; k++) {
         for (l = 0; l < l_Temp; l++) {
-            X[l][k][0] = X_low[k][l + t_HFAdj][0];
-            X[l][k][1] = X_low[k][l + t_HFAdj][1];
+            X[l][k][0] = X_low[k][l + ENVELOPE_ADJUSTMENT_OFFSET][0];
+            X[l][k][1] = X_low[k][l + ENVELOPE_ADJUSTMENT_OFFSET][1];
         }
     }
     for (; k < sbr->k[4] + sbr->mold; k++) {
         for (l = 0; l < l_Temp; l++) {
-            X[l][k][0] = Y[1][k][l + t_HFAdj + l_f][0];
-            X[l][k][1] = Y[1][k][l + t_HFAdj + l_f][1];
+            X[l][k][0] = Y[1][k][l + ENVELOPE_ADJUSTMENT_OFFSET + l_f][0];
+            X[l][k][1] = Y[1][k][l + ENVELOPE_ADJUSTMENT_OFFSET + l_f][1];
         }
     }
 
     for (k = 0; k < sbr->k[3]; k++) {
         for (l = l_Temp; l < l_f; l++) {
-            X[l][k][0] = X_low[k][l + t_HFAdj][0];
-            X[l][k][1] = X_low[k][l + t_HFAdj][1];
+            X[l][k][0] = X_low[k][l + ENVELOPE_ADJUSTMENT_OFFSET][0];
+            X[l][k][1] = X_low[k][l + ENVELOPE_ADJUSTMENT_OFFSET][1];
         }
     }
     for (; k < sbr->k[3] + sbr->m; k++) {
         for (l = l_Temp; l < l_f; l++) {
-            X[l][k][0] = Y[0][k][l + t_HFAdj][0];
-            X[l][k][1] = Y[0][k][l + t_HFAdj][1];
+            X[l][k][0] = Y[0][k][l + ENVELOPE_ADJUSTMENT_OFFSET][0];
+            X[l][k][1] = Y[0][k][l + ENVELOPE_ADJUSTMENT_OFFSET][1];
         }
     }
     return 0;
