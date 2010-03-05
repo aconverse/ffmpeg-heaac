717496d56882b157a366a11397fb8fdd058add31
Alex Converse <alex.converse@gmail.com>
2010-03-05 17:59:17 -0500

    sbr_chirp(): Simplify new_bw calculation

--- libavcodec/aacsbr.c
+++ libavcodec/aacsbr.c
@@ -1312,28 +1312,13 @@ static void sbr_chirp(SpectralBandReplication *sbr, SBRData *ch_data)
     int i;
     float new_bw;
     float temp_bw;
+    static const float bw_tab[] = { 0.0f, 0.75f, 0.9f, 0.98f };
 
     for (i = 0; i < sbr->n_q; i++) {
-        switch (ch_data->bs_invf_mode[0][i]) {
-        case 0:
-            if (ch_data->bs_invf_mode[1][i] == 1) {
-                new_bw = 0.6f;
-            } else
-                new_bw = 0.0f;
-            break;
-        case 1:
-            if (!ch_data->bs_invf_mode[1][i]) {
-                new_bw = 0.6f;
-            } else
-                new_bw = 0.75f;
-            break;
-        case 2:
-            new_bw = 0.9f;
-            break;
-        default:
-            new_bw = 0.98f;
-            break;
-        }
+        if (ch_data->bs_invf_mode[0][i] + ch_data->bs_invf_mode[1][i] == 1) {
+            new_bw = 0.6f;
+        } else
+            new_bw = bw_tab[ch_data->bs_invf_mode[0][i]];
 
         if (new_bw < ch_data->bw_array[i]) {
             temp_bw = 0.75f    * new_bw + 0.25f    * ch_data->bw_array[i];
