474cb6588269246d4bded82f99c17abc4132aa47
Alex Converse <alex.converse@gmail.com>
2010-03-05 18:02:47 -0500

    sbr_chirp(): remove an unneeded variable

--- libavcodec/aacsbr.c
+++ libavcodec/aacsbr.c
@@ -1311,7 +1311,6 @@ static void sbr_chirp(SpectralBandReplication *sbr, SBRData *ch_data)
 {
     int i;
     float new_bw;
-    float temp_bw;
     static const float bw_tab[] = { 0.0f, 0.75f, 0.9f, 0.98f };
 
     for (i = 0; i < sbr->n_q; i++) {
@@ -1321,10 +1320,10 @@ static void sbr_chirp(SpectralBandReplication *sbr, SBRData *ch_data)
             new_bw = bw_tab[ch_data->bs_invf_mode[0][i]];
 
         if (new_bw < ch_data->bw_array[i]) {
-            temp_bw = 0.75f    * new_bw + 0.25f    * ch_data->bw_array[i];
+            new_bw = 0.75f    * new_bw + 0.25f    * ch_data->bw_array[i];
         } else
-            temp_bw = 0.90625f * new_bw + 0.09375f * ch_data->bw_array[i];
-        ch_data->bw_array[i] = temp_bw < 0.015625f ? 0.0f : temp_bw;
+            new_bw = 0.90625f * new_bw + 0.09375f * ch_data->bw_array[i];
+        ch_data->bw_array[i] = new_bw < 0.015625f ? 0.0f : new_bw;
     }
 }
 
