b7a7fe113e42757023fbc5a3746f01692acca9f0
Alex Converse <alex.converse@gmail.com>
2010-03-05 06:23:51 -0500

    array_min_int16(): Use FFMIN().

--- libavcodec/aacsbr.c
+++ libavcodec/aacsbr.c
@@ -273,8 +273,7 @@ static int array_min_int16(int16_t *array, int nel)
 {
     int i, min = array[0];
     for (i = 1; i < nel; i++)
-        if (array[i] < min)
-            min = array[i];
+        min = FFMIN(array[i], min);
     return min;
 }
 
