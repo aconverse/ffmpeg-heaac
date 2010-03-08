9f14af5e8c16256f4c2c5959930d69c787694566
Alex Converse <alex.converse@gmail.com>
2010-03-07 22:03:12 -0500

    Document some context variables

--- libavcodec/sbr.h
+++ libavcodec/sbr.h
@@ -83,17 +83,26 @@ typedef struct {
     DECLARE_ALIGNED(16, float, synthesis_filterbank_samples)[SBR_SYNTHESIS_BUF_SIZE];
     DECLARE_ALIGNED(16, float, analysis_filterbank_samples) [1312];
     int                synthesis_filterbank_samples_offset;
+    ///l_APrev and l_A
     int                e_a[2];
+    ///Chirp factors
     float              bw_array[5];
+    ///QMF values of the original signal
     float              W[2][32][32][2];
+    ///QMF output of the HF adjustor
     float              Y[2][38][64][2];
     float              g_temp[42][48];
     float              q_temp[42][48];
     uint8_t            s_indexmapped[8][48];
+    ///Envelope scalefactors
     float              env_facs[6][48];
+    ///Noise scalefactors
     float              noise_facs[3][5];
+    ///Envelope time borders
     uint8_t            t_env[8];
+    ///Envelope time border of the last envelope of the previous frame
     uint8_t            t_env_num_env_old;
+    ///Noise time borders
     uint8_t            t_q[3];
     unsigned           f_indexnoise;
     unsigned           f_indexsine;
@@ -120,31 +129,54 @@ typedef struct {
     /** @} */
     unsigned           bs_coupling;
     unsigned           k[5]; ///< k0, k1, k2
-    unsigned           kx[2];///< kx', and kx respectively
-    unsigned           m[2]; ///< M' and M respectively
+    ///kx', and kx respectively, kx is the first QMF subband where SBR is used.
+    ///kx' is its value from the previous frame
+    unsigned           kx[2];
+    ///M' and M respectively, M is the number of QMF subbands that use SBR.
+    unsigned           m[2];
+    ///The number of frequency bands in f_master
     unsigned           n_master;
     SBRData            data[2];
-    unsigned           n[2]; ///< n_low and n_high respectively
+    ///N_Low and N_High respectively, the number of frequency bands for low and high resolution
+    unsigned           n[2];
+    ///Number of noise floor bands
     unsigned           n_q;
+    ///Number of limiter bands
     unsigned           n_lim;
+    ///The master QMF frequency grouping
     uint16_t           f_master[49];
+    ///Frequency borders for low resolution SBR
     uint16_t           f_tablelow[25];
+    ///Frequency borders for high resolution SBR
     uint16_t           f_tablehigh[49];
+    ///Frequency borders for noise floors
     uint16_t           f_tablenoise[6];
+    ///Frequency borders for the limiter
     uint16_t           f_tablelim[29];
     unsigned           num_patches;
     uint8_t            patch_num_subbands[6];
     uint8_t            patch_start_subband[6];
+    ///QMF low frequency input to the HF generator
     float              X_low[32][40][2];
+    ///QMF output of the HF generator
     float              X_high[64][40][2];
+    ///QMF values of the reconstructed signal
     DECLARE_ALIGNED(16, float, X)[2][32][64];
+    ///Zeroth coefficient used to filter the subband signals
     float              alpha0[64][2];
+    ///First coefficient used to filter the subband signals
     float              alpha1[64][2];
+    ///Dequantized envelope scalefactors, remapped
     float              e_origmapped[7][48];
+    ///Dequantized noise scalefactors, remapped
     float              q_mapped[7][48];
+    ///Sinusoidal presence, remapped
     uint8_t            s_mapped[7][48];
+    ///Estimated envelope
     float              e_curr[7][48];
+    ///Amplitude adjusted noise scalefactors
     float              q_m[7][48];
+    ///Sinusoidal levels
     float              s_m[7][48];
     float              gain[7][48];
     DECLARE_ALIGNED(16, float, qmf_filter_scratch)[5][64];
