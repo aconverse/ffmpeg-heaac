aconverse/ffmpeg-heaac

commit 2c49a8b66eff0938e9f1a0e2712d6f5fc482e7ae

Combine the two fix up envelope cases.

libavcodec/ps.c
@@ -283,20 +283,7 @@ int ff_ps_read_data(AVCodecContext *avctx, GetBitContext *gb_host, PSContext *ps
     ps->enable_ipdopd &= !PS_BASELINE;
 
     //Fix up envelopes
-    if (!ps->num_env) {
-        ps->num_env = 1;
-        ps->border_position[1] = 31;
-        if (ps->enable_iid && ps->num_env_old > 1) {
-            memcpy(ps->iid_par, ps->iid_par+ps->num_env_old-1, sizeof(ps->iid_par[0]));
-        }
-        if (ps->enable_icc && ps->num_env_old > 1) {
-            memcpy(ps->icc_par, ps->icc_par+ps->num_env_old-1, sizeof(ps->icc_par[0]));
-        }
-        if (ps->enable_ipdopd && ps->num_env_old > 1) {
-            memcpy(ps->ipd_par, ps->ipd_par+ps->num_env_old-1, sizeof(ps->ipd_par[0]));
-            memcpy(ps->opd_par, ps->opd_par+ps->num_env_old-1, sizeof(ps->opd_par[0]));
-        }
-    } else if (ps->border_position[ps->num_env] < numQMFSlots - 1) {
+    if (!ps->num_env || ps->border_position[ps->num_env] < numQMFSlots - 1) {
         //Create a fake envelope
         if (ps->enable_iid && ps->num_env_old > 1) {
             memcpy(ps->iid_par+ps->num_env, ps->iid_par+ps->num_env-1, sizeof(ps->iid_par[0]));
@@ -304,7 +291,7 @@ int ff_ps_read_data(AVCodecContext *avctx, GetBitContext *gb_host, PSContext *ps
         if (ps->enable_icc && ps->num_env_old > 1) {
             memcpy(ps->icc_par+ps->num_env, ps->icc_par+ps->num_env-1, sizeof(ps->icc_par[0]));
         }
-        if (ps->enable_ipdopd) {
+        if (ps->enable_ipdopd && ps->num_env_old > 1) {
             memcpy(ps->ipd_par+ps->num_env, ps->ipd_par+ps->num_env-1, sizeof(ps->ipd_par[0]));
             memcpy(ps->opd_par+ps->num_env, ps->opd_par+ps->num_env-1, sizeof(ps->opd_par[0]));
         }

commit efff4e2a681866d11c99a5a384dd4b6a9a8ffa29

Fix a regression from 2c49a8b66eff0938e9f1a0e2712d6f5fc482e7ae.
Fixup for ps->num_env = 0 was using the wrong source.

libavcodec/ps.c
@@ -285,15 +285,18 @@ int ff_ps_read_data(AVCodecContext *avctx, GetBitContext *gb_host, PSContext *ps
     //Fix up envelopes
     if (!ps->num_env || ps->border_position[ps->num_env] < numQMFSlots - 1) {
         //Create a fake envelope
+        int source = ps->num_env ? ps->num_env - 1 : ps->num_env_old - 1;
+        if (source >= 0 && source != ps->num_env) {
         if (ps->enable_iid && ps->num_env_old > 1) {
-            memcpy(ps->iid_par+ps->num_env, ps->iid_par+ps->num_env-1, sizeof(ps->iid_par[0]));
+            memcpy(ps->iid_par+ps->num_env, ps->iid_par+source, sizeof(ps->iid_par[0]));
         }
         if (ps->enable_icc && ps->num_env_old > 1) {
-            memcpy(ps->icc_par+ps->num_env, ps->icc_par+ps->num_env-1, sizeof(ps->icc_par[0]));
+            memcpy(ps->icc_par+ps->num_env, ps->icc_par+source, sizeof(ps->icc_par[0]));
         }
         if (ps->enable_ipdopd && ps->num_env_old > 1) {
-            memcpy(ps->ipd_par+ps->num_env, ps->ipd_par+ps->num_env-1, sizeof(ps->ipd_par[0]));
-            memcpy(ps->opd_par+ps->num_env, ps->opd_par+ps->num_env-1, sizeof(ps->opd_par[0]));
+            memcpy(ps->ipd_par+ps->num_env, ps->ipd_par+source, sizeof(ps->ipd_par[0]));
+            memcpy(ps->opd_par+ps->num_env, ps->opd_par+source, sizeof(ps->opd_par[0]));
+        }
         }
         ps->num_env++;
         ps->border_position[ps->num_env] = numQMFSlots - 1;